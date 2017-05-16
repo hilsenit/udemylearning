8c70ae4e28e422c15e04d03ad5b6b84f7f5e7964
peterhoejlundandersen <hilsenit@gmail.com>
2017-05-16 17:02:06 +0200

    delete route added

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,10 @@ def index
 	@portfolios_items = Portfolio.all	
 end	
 
+def show
+	@portfolio_item = Portfolio.find(params[:id])
+end
+
 def new
 	@portfolio_item = Portfolio.new
 end
@@ -38,6 +42,15 @@ def update
 	end
 end
 
+
+def destroy
+	@portfolio_item = Portfolio.find(params[:id])
+	@portfolio_item.destroy
+	respond_to do |format|
+		format.html { redirect_to portfolios_path, notice: "Portfolio_item was removed."}
+
+	end	
+end
 private 
 
 def portfolio_params
