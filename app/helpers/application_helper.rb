bfa573c84c995eefa495be2150075398c5c4694d
peterhoejlundandersen <hilsenit@gmail.com>
2017-05-31 19:18:49 +0200

    implemented view helper for current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def login_helper
+		if current_user.is_a?(User)
+		 link_to "Log ud", destroy_user_session_path, 
+		 					method: :delete, 
+		 					class: "button primary"
+		else
+		(link_to "Registrer", new_user_registration_path, 
+		 					class: "button primary") +
+		 (link_to "Log ind", new_user_session_path, 
+		  		 					class: "button primary") 
+		end
+	end
 end
